e29eb1734337bdde872321683039c162157f6e14
mariannedevriendt <devriendt.marianne@gmail.com>
2017-12-06 19:00:30 +0100

    projet a fusionner

--- main.cpp
+++ main.cpp
@@ -19,67 +19,7 @@
 
 
 int main(int argc, char *argv[]){
-
-        MatrixXf origin(rows,col);
-        for(int i=0; i<rows;i++){
-            for(int j=0;j<col;j++){
-                origin(i,j)=2.0+j+i;
-            }
-        }
-        cout<< mean(origin, 1)<<endl;
-        cout<<origin<<endl;
-        VectorXf means(col);
-        adjust_data(origin,means);
-        cout<< "data ajustee" <<endl;
-
-        MatrixXf covar_matrix(col,col);
-
-        compute_covariance_matrix(origin,covar_matrix);
-        cout<< "covariance calculee" <<endl;
-        cout<< covar_matrix<<endl;
-
-        //int dim = col;
-
-
-        EigenSolver<MatrixXf> eig = eigen(covar_matrix);
-        cout<<"The eigenvalues"<<endl;
-        cout<<eig.eigenvalues().transpose()<<endl;
-        cout<<"The eigenvectors"<<endl;
-
-       VectorXf eigenVal(col);
-       MatrixXf final_data(rows,col);
-       MatrixXf eigenVec(col,col);
-       for(int i=0; i<col;i++){
-           eigenVal(i)= eig.eigenvalues()[i].real();
-           for(int j=0; j<col;j++){
-               eigenVec(i,j)= eig.eigenvectors().col(j)[i].real();
-           }
-       }
-       cout<<eigenVec<<endl;
-
-       MatrixXf finalEigenVec = decreaseDim(eigenVec, eigenVal, 0.95);
-       int nbVec= finalEigenVec.cols();
-       cout<<"vecteurs propres réduits"<<endl;
-       cout<<finalEigenVec<<endl;
-
-       MatrixXf baseChange(rows,col);
-       MatrixXf transposeEigen(col,col);
-       transpose(eigenVec,transposeEigen);
-       multiply(origin,transposeEigen,baseChange);
-
-       MatrixXf final(rows,nbVec);
-       for(int i=0; i<rows;i++){
-           for(int j=0; j<nbVec;j++){
-               final(i,j)=baseChange(i,j);
-           }
-       }
-       cout<<"donnees finales "<<endl;
-       cout<<final<<endl;
-
-       writeToText("test", final);
-
-
-return 0;
-
-
+        std::string c = "/Users/mariannedevriendt/Desktop/results.txt";
+        principalComponentAnalysis(c);
+        return 0;
 }
--- pca.cpp
+++ pca.cpp
@@ -1,4 +1,5 @@
 #include "pca.h"
+#include <vector>
 
 using namespace std;
 using namespace Eigen;
@@ -8,8 +9,9 @@ pca::pca()
 
 }
 
-const int rows=10;
-const int col = 10;
+int rows=20;
+int col=10;
+float parameter = 0.98;
 
 double mean(MatrixXf& origin, int i){
     double mean =0;
@@ -31,37 +33,31 @@ double variance(MatrixXf&  origin, double& mean,int i){
     return variance;
 }
 
-//Dans le cas ou on centre et reduit la matrice d'origine
-/*double* transformation(double* origin){
-    newMat= new double* [rows];
-    for(int i=0; i<col; i++){
-        double m = mean(origin,i);
-        double v = variance(origin,m,i);
-        for(int j=0; j<rows; j++){
-            newMat[j] = new double*[col];
-            newMat[j][i]= double(origin[i][j]-m)/v;
-        }
-    }
-    return newMat;
-}*/
 
 //le tableau mean sert à stocker les moyennes
 //on calcule les moyennes des variables aleatoires et on les centre
-void adjust_data(MatrixXf&origin, VectorXf& means){
+void adjust_data(MatrixXf& origin, VectorXf& means){
     for(int i=0; i<col; i++){
-        double mean =0;
+        float mean =0;
         for(int j=0; j<rows;j++){
-            mean+= origin(i,j);
+            mean+= origin(j,i);
         }
-        mean = (double) mean/rows;
+
+        mean = (float) mean/rows;
         means(i) = mean;
+
+        double norm=0;
         for(int j=0;j<rows;j++){
-            origin(j,i)-=mean;
+            origin(j,i)=origin(j,i) - mean;
+            norm+= pow(origin(j,i),2);
+        }
+
+        for(int j=0; j<rows;j++){
+            if(norm==0){origin(j,i)=0;}
+            else{ origin(j,i)= (float) (origin(j,i)/sqrt(norm));}
         }
     }
 }
-
-
 
 double compute_covariance(const MatrixXf& d, int i, int j){
     double cov=0;
@@ -90,14 +86,6 @@ void compute_covariance_matrix( const MatrixXf& d, MatrixXf& covar_mat){
 }
 
 //Calcul des valeurs et vecteurs propres de la matrice de covariance
-/*void eigen(MatrixXf& covar_mat, MatrixXf& eigenvector, VectorXf& eigenvalues){
-    //Eigenvalue<double> eig(covar_matrix);
-
-    eig.getV(eigenvector);
-    eig.getD(eigenvalues);
-}*/
-
-
 EigenSolver<MatrixXf> eigen( MatrixXf& origin){
     EigenSolver<MatrixXf> es;
     es.compute(origin, true);
@@ -105,19 +93,18 @@ EigenSolver<MatrixXf> eigen( MatrixXf& origin){
 }
 
 void transpose(MatrixXf& origin, MatrixXf& transp){
-    for(int i=0; i<col;i++){
-        for(int j=0; j<rows; j++){
+    for(int i=0; i<origin.cols();i++){
+        for(int j=0; j<origin.rows(); j++){
             transp(i,j)=origin(j,i);
         }
     }
 }
 
-
 //retourne A*B dans C
 void multiply(MatrixXf& A ,MatrixXf& B, MatrixXf& C){
     int a = A.rows();
-    int b = B.cols();
     int n = A.cols();
+    int b = B.cols();
     if(n!=(int)B.rows()){cout<<"erreur de dimension"<<endl;}
     else{
         for(int i=0; i<a; i++){
@@ -130,7 +117,7 @@ void multiply(MatrixXf& A ,MatrixXf& B, MatrixXf& C){
     }
 }
 
-MatrixXf decreaseDim(MatrixXf& eigenVec, VectorXf& eigenVal, float parameter){
+int decreaseDim(VectorXf& eigenVal){
     float s=0;
     for(int i=0; i<eigenVal.size(); i++){
         s+=eigenVal(i);
@@ -143,23 +130,9 @@ MatrixXf decreaseDim(MatrixXf& eigenVec, VectorXf& eigenVal, float parameter){
         sum+=eigenVal(indice);
         indice++;
     }
-    int r= eigenVec.rows();
-    MatrixXf final(r,indice) ;
-
-    for(int i=0;i<r;i++){
-        for(int j=0;j<indice;j++){
-            final(i,j)=eigenVec(i,j);
-        }
-    }
-    return final;
-
-
+    return indice;
 }
 
-/*void writeToCSVfile(string name, MatrixXf matrix){
-    ofstream file(name.c_str());
-    file<<matrix.format(CSVFormat);
-}*/
 
 void writeToText(string name, MatrixXf matrix){
     std::ofstream file(name+".csv");
@@ -171,21 +144,104 @@ void writeToText(string name, MatrixXf matrix){
             }
             file<<endl;
         }
-       // file<<matrix;
     }
 }
 
+template<typename M>
+M load_csv (const std::string & path) {
+    std::ifstream indata;
+    indata.open(path);
+    std::string line;
+    std::vector<float> values;
+    int r = 0;
 
-/*//calcul de la matrice des correlations a partir d'une matrice centrée réduite
-double* correlation( double* reduced){
-    int k = reduced.length;
-    double*  treduced= transpose(reduced);
-    double* C = multiply(treduced,reduced);
-    for(int i=0; i<C.length;i++){
-        for(int j=0; j<C[0].length; j++){
-            C[i][j]=C[i][j] * (double)(1/k);
+    while (std::getline(indata, line)) {
+        int colonnes=0;
+        std::stringstream lineStream(line);
+        std::string cell;
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+            colonnes+=1;
+
+        }
+        col=colonnes-1;
+        ++r;
+    }
+
+    rows=r;
+    return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, RowMajor>>(values.data(), r, values.size()/r);
+}
+
+void principalComponentAnalysis(const std::string& pathData){
+    //MatrixXf origin = load_csv<MatrixXf>(pathData);
+
+     MatrixXf origin2 = load_csv<MatrixXf>(pathData);
+     MatrixXf origin(rows,col);
+
+     cout<< "lignes origin " << origin2.rows() << "  "<< rows<<endl;
+     cout<<"colonnes origin "<<origin2.cols()<<"  "<< col <<endl;
+     for(int i=0; i<rows;i++){
+     for(int j=0;j<col;j++){
+     origin(i,j)=origin2(i,j+1);
+     //origin(i,j)=rand();
+     }
+     }
+    VectorXf means(col);
+    adjust_data(origin,means);
+    MatrixXf covar_matrix(col,col);
+    compute_covariance_matrix(origin,covar_matrix);
+
+    cout<< "covariance calculee" <<endl;
+    cout<< covar_matrix<<endl;
+    cout<<" covar rows "<< covar_matrix.rows()<<endl;
+    cout<<"covar cols "<<covar_matrix.cols()<<endl;
+
+
+    EigenSolver<MatrixXf> eig = eigen(covar_matrix);
+    VectorXf eigenVal(col);
+    MatrixXf final_data(rows,col);
+    MatrixXf eigenVec(col,col);
+
+    cout<<"The eigenvalues"<<endl;
+    cout<<eig.eigenvalues().transpose()<<endl;
+    cout<<"The eigenvectors"<<endl;
+
+    for(int i=0; i<col;i++){
+        eigenVal(i)= eig.eigenvalues()[i].real();
+        for(int j=0; j<col;j++){
+            eigenVec(i,j)= eig.eigenvectors().col(j)[i].real();
         }
     }
-    return C;
-}*/
 
+
+    cout<<eigenVec<<endl;
+
+    int nbVec= decreaseDim(eigenVal);
+    cout<<"nbVec "<<nbVec<<endl;
+    cout<<"vecteurs propres réduits"<<endl;
+
+    MatrixXf baseChange(rows,col);
+    MatrixXf transposeEigen(col,col);
+    transpose(eigenVec,transposeEigen);
+    cout<<"transpose Eigen "<< transposeEigen<<endl;
+    cout<< "origin rows cols "<< origin.rows() <<" "<<origin.cols()<<endl;
+    cout<< "transEig rows cols "<< transposeEigen.rows() <<" "<<transposeEigen.cols()<<endl;
+    cout<< "basChaneg rows cols "<< baseChange.rows() <<" "<<baseChange.cols()<<endl;
+    multiply(origin,transposeEigen,baseChange);
+    cout<<"baseChange "<<endl;
+    cout<<baseChange<<endl;
+
+    MatrixXf final(rows,nbVec);
+    for(int i=0; i<rows;i++){
+        for(int j=0; j<nbVec;j++){
+            final(i,j)=baseChange(i,j);
+        }
+    }
+    cout<<"donnees finales "<<endl;
+    cout<<final<<endl;
+
+    cout<<"lignes finales "<< final.rows()<<endl;
+    cout<<"colonnes finales "<< final.cols()<<endl;
+
+    writeToText("resultDimReduction", final);
+}
